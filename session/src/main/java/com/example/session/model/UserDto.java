8ccdcb6ad8af8ed7e3287c3ab5ea48524aac6ccd
IMWoo94 <leemeawoo94@gmail.com>
2024-01-21 15:30:36 +0900

    feat : 사용자 DTO 생성

---
+++ session/src/main/java/com/example/session/model/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.session.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+	private String name;
+	private String password;
+}
