862545c9fbc132496c316538a872cc1b30554d10
IMWoo94 <leemeawoo94@gmail.com>
2024-01-21 17:49:41 +0900

    feat : 로그인 시 전달받은 Request 생성

---
+++ cookie/src/main/java/com/example/cookie/model/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.example.cookie.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginRequest {
+
+	private String id;
+	private String password;
+}
