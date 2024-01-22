d488d7e50eac6b31ee4ba43ee5193377c07f8ad1
IMWoo94 <leemeawoo94@gmail.com>
2024-01-22 16:27:19 +0900

    test : jwt token 검증 테스트
    1. 정상 케이스
    2. 유효기간 만료 예외 확인
    3. 변조 예외 확인

--- jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
+++ jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -24,7 +24,8 @@ class JwtApplicationTests {
 		claims.put("user_id", 923);
 
 		// 현재시간 + 10분 유효
-		var expireAt = LocalDateTime.now().plusMinutes(10);
+		// var expireAt = LocalDateTime.now().plusMinutes(10);
+		var expireAt = LocalDateTime.now().plusSeconds(20);
 
 		var jwtToken = jwtService.create(claims, expireAt);
 
@@ -33,7 +34,19 @@ class JwtApplicationTests {
 
 	@Test
 	void tokenValidation(){
+		var token = "eaJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5MjMsImV4cCI6MTcwNTkwODMwNH0.ZPtYmd_nQz5Uc1S1uXOzbri4CptCELPJUl5hhYGBTGc";
 
+		// 토큰 만료 예외
+		/**
+		 * io.jsonwebtoken.ExpiredJwtException: JWT expired at 2024-01-22T07:25:04Z. Current time: 2024-01-22T07:25:07Z, a difference of 3811 milliseconds.  Allowed clock skew: 0 milliseconds.
+		 */
+
+		// 토큰 변조 예외
+		/**
+		 * io.jsonwebtoken.MalformedJwtException: Malformed JWT JSON
+		 * Caused by: io.jsonwebtoken.io.DeserializationException: Unable to deserialize bytes into a java.lang.Object instance: Unrecognized token 'y': was expecting (JSON String, Number, Array, Object or token 'null', 'true' or 'false')
+		 */
+		jwtService.validation(token);
 	}
 
 }
