99ddaa258b459e16d0f999deea027fc2dbcc64f3
IMWoo94 <leemeawoo94@gmail.com>
2024-01-22 16:20:00 +0900

    test : jwt token 생성 테스트

--- jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
+++ jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.jwt;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.jwt.service.JwtService;
 
 @SpringBootTest
 class JwtApplicationTests {
+	@Autowired
+	private JwtService jwtService;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void tokenCreate(){
+		var claims = new HashMap<String, Object>();
+		claims.put("user_id", 923);
+
+		// 현재시간 + 10분 유효
+		var expireAt = LocalDateTime.now().plusMinutes(10);
+
+		var jwtToken = jwtService.create(claims, expireAt);
+
+		System.out.println(jwtToken);
+	}
+
+	@Test
+	void tokenValidation(){
+
 	}
 
 }
